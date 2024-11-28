774e45809a2a5d2700b61d0b2575c195531c889c
Alumnado1DAM <eliaspm05@gmail.com>
2024-11-28 13:07:41 +0100

    Agrego proyecto 20 numeros randoms

---
+++ src/NUMEROS_RANDOMS/RandomNumbers.java
@@ -0,0 +1,19 @@
+package NUMEROS_RANDOMS;
+
+package NUMEROS_RANDOMS;
+
+import java.util.Random;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        generateRandomNumbers();
+    }
+
+    public static void generateRandomNumbers() {
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            int randomNumber = 1 + random.nextInt(10);
+            System.out.println(randomNumber);
+        }
+    }
+}
